1bbb6eb7861dd7ccba7187b261165941f51b4c7e
Tommy <tommy@example.com>
2025-09-01 16:11:07 +0800

    添加访问的Hosts配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
+/*
+ * @Author: Tommy tommy@example.com
+ * @Date: 2025-08-29 16:18:36
+ * @LastEditors: Tommy tommy@example.com
+ * @LastEditTime: 2025-09-01 16:10:42
+ * @FilePath: \drop_admin\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
    server: {
     host: '0.0.0.0',
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
