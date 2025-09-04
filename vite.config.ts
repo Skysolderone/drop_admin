d4efd5421c79fca0f9d57b316db133045335a9e0
arthur <arthur@dropwallet.world>
2025-09-04 19:48:01 +0800

    更换域名

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-const isProduction = process.env.NODE_ENV === 'production'
+// const isProduction = process.env.NODE_ENV === 'production'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -25,7 +25,7 @@ export default defineConfig({
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/v1': {
-        target: isProduction ? 'http://drop-api-py:9393' : 'http://worldchain_drop_api:9393',
+        target:  'http://drop-api-py:9393',
         changeOrigin: true,
       },
       '/uploads': {
